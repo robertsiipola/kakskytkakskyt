20c5509711a0e9d6fbd2ba93853214ad687b5fc5
robertsiipola <robert@three.consulting>
2020-06-30 10:21:58 +0300

    added Record<never, never> to the Main component

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './main.module.css';
 
-const Main: React.FC<> = ({ children }) => {
+const Main: React.FC<Record<never, never>> = ({ children }) => {
     return <div className={styles.main}>{children}</div>;
 };
 
