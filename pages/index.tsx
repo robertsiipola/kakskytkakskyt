3e2abc986be178275c2db1d2b228af40caadad82
robertsiipola <robert@three.consulting>
2020-06-30 10:52:07 +0300

    oops

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Title from '../components/Title';
 import useContentful from '../utils/hooks/useContentful';
 import ReactMarkdown from 'react-markdown';
 
-const IndexPage: React.FC<Record<string, unknown>> = () => {
+const IndexPage: React.FC<unknown> = () => {
     const [data, loading, error] = useContentful(process.env.NEXT_PUBLIC_MANIFEST_ID);
 
     if (loading) return <div>Loading...</div>;
