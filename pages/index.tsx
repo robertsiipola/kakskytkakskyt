96fcdc8732429896f44277732fb68b55f3f7a9d0
robertsiipola <robert@three.consulting>
2020-07-01 11:42:03 +0300

    added better font scaling parameters

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Title from '../components/Title';
 import useContentful from '../utils/hooks/useContentful';
 import ReactMarkdown from 'react-markdown';
+import styles from '../styles/index.module.css';
 
 const IndexPage: React.FC<Record<never, never>> = () => {
     const [data, loading, error] = useContentful();
@@ -15,7 +16,7 @@ const IndexPage: React.FC<Record<never, never>> = () => {
             <div>
                 {data && (
                     <>
-                        <h2>{data.title}</h2>
+                        <h2 className={styles.title}>{data.title}</h2>
                         <div>
                             <ReactMarkdown source={data.body} />
                         </div>
